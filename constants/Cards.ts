778d234f884748c48a13321fa2a8c7f6b10781df
Andre <opolloandres@gmail.com>
2024-06-04 20:21:00 +0300

    [new]create Card List

---
+++ constants/Cards.ts
@@ -0,0 +1,19 @@
+export const cards = [
+    { source:require('../assets/cards/chariot.png') },
+    { source:require('../assets/cards/death.png') },
+    { source:require('../assets/cards/devil.png') },
+    { source:require('../assets/cards/fool.png') },
+    { source:require('../assets/cards/hermit.png') },
+    { source:require('../assets/cards/high-priestess.png') },
+    { source:require('../assets/cards/judegment.png') },
+    { source:require('../assets/cards/justice.png') },
+    { source:require('../assets/cards/lover.png') },
+    { source:require('../assets/cards/moon.png') },
+    { source:require('../assets/cards/pendu.png') },
+    { source:require('../assets/cards/strength.png') },
+    { source:require('../assets/cards/sun.png') },
+    { source:require('../assets/cards/temperance.png') },
+    { source:require('../assets/cards/tower.png') },
+    { source:require('../assets/cards/wheel.png') },
+    { source:require('../assets/cards/world.png') }
+]
